7243a4749860fc81768487091ff9a91dcf5bf245
seankao <seankao@rayark.com>
2025-01-26 15:35:35 +0800

    Add exit button.

--- Assets/Scripts/MainFsm/MatchProcess.cs
+++ Assets/Scripts/MainFsm/MatchProcess.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace CliffLeeCL
 {
     public class MatchProcess : State<GameCore>
     {
+        InputSystem_Actions inputActions;
 
         public override void UpdateState()
         {
+            inputActions.Enable();
+            if (inputActions.Player.Exit.triggered)
+            {
+                Debug.LogError("Exit");
+                stateContext.matchRoundTimer.StopCountDownTimer();
+                EventManager.Instance.OnMatchOver();
+                SceneManager.UnloadScene(SceneManager.GetSceneByName("Game"));
+                stateContext.SwitchState("MainMenu");
+
+            }
+            if (inputActions.Player.Attack.triggered)
+            {
+                Debug.LogError("AUU");
+            }
         }
 
         public override void OnStateEnter()
         {
+            inputActions = new InputSystem_Actions();
+            inputActions.Enable();
+
             stateContext.matchStartTimer.StartCountDownTimer(stateContext.matchStartTime, false, OnMatchStart);
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
